7c8146595d772db648b918b6eadfd3b081a34dba
PhilippePH <55930314+PhilippePH@users.noreply.github.com>
2023-06-29 10:29:24 +0100

    Stealth Plugin and setUpBrowser function

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 const selectWebsites = require('./websiteSelection.cjs');
-const puppeteer = require("puppeteer"); /// change this to puppeteer core!!!!!!!!!!
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+// register `puppeteer-extra` plugins.
+puppeteer.use(StealthPlugin()) // allows to pass all tests on SannySoft, even if not *actually* in headfull mode
 
 const NUM_URLS = 10;
+const browser_list = ['Chrome']
 
-// async waits for something to finish before moving on
-
-async function crawl(){
-    data = await selectWebsites.getFirstURLs(NUM_URLS);
-    // console.log(data);
-
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-
-    // loop through URLs
-    for(let i = 0; i < data.length; i++){
-        let URL = data[i];
-        console.log(URL);
-        try{
-            await page.goto(URL);
-            await page.screenshot({path: "./screenshots/"+i+".png"});
-        } catch(error){
-            console.log("Couldn't open" + URL);
-            console.log(error);
-        }
-    }
-
-    await browser.close();
+async function setUpBrowser(TBD){
+    let browser = puppeteer.launch({
+        headless: false, // allows to pass most tests on SannySoft, except WebDrive
+        ignoreHTTPSErrors: true,
+        args: ['--start-maximized'] // browser takes whole screen
+    });
+    return browser;
 }
-crawl();
+
+async function crawl(browser_list){
+    // set up URL List
+    // URL_list = await selectWebsites.getFirstURLs(NUM_URLS);
+    URL_list = ["https://bot.sannysoft.com/"];
+    
+    for(let j = 0; j < browser_list.length; j++){
+        const browser = await setUpBrowser(browser_list[j]);
+        const page = await browser.newPage();
+
+        // loop through URLs
+        for(let i = 0; i < URL_list.length; i++){
+            let URL = URL_list[i];
+            console.log(URL);
+            try{
+                await page.goto(URL,{
+                    waitUntil: "domcontentloaded", // need to double check docs here
+                });
+
+
+                await page.screenshot({path: "./screenshots/"+i+".png"});
+            } catch(error){
+                console.log("Couldn't open" + URL);
+                console.log(error);
+            }
+        }
+
+        await browser.close();
+    }
+}
+
+
+crawl(browser_list);
