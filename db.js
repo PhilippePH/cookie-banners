499f0d596a26401c03c760148f8d5cd6c67455a8
PhilippePH <55930314+PhilippePH@users.noreply.github.com>
2023-06-29 16:55:24 +0100

    saving cookies to sql database

--- db.js
+++ db.js
@@ -1,24 +1,64 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'myTest',
-});
+// const connection = mysql.createConnection({
+//   host: '127.0.0.1',
+//   user: 'root',
+//   password: 'I@mastrongpsswd',
+//   database: 'CrawlData',
+// });
 
-connection.connect(function(err) {
+async function establishConnection(connection){
+  connection.connect(function(err) {
     if (err) {
       return console.error('error: ' + err.message);
     }
   
     console.log('Connected to the MySQL server.');
   });
-  
-connection.end(function(err) {
-    if (err) {
-      return console.log('error:' + err.message);
-    }
-    console.log('Close the database connection.');
-  });
-  
+}
+
+async function endConnection(connection){
+  connection.end(function(err) {
+      if (err) {
+        return console.log('error:' + err.message);
+      }
+      console.log('Close the database connection.');
+    });
+}
+
+async function saveCookies(URL, cookies){
+  const insertDataQuery = 'INSERT INTO cookie_data (name, value, domain, path, expires, size, httpOnly, secure, session, sameSite, sameParty, sourceScheme, sourcePort) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+  for(let i = 0; i < cookies.length; i++){ // later on see if we can do a "batch add" and add all the lines at once (assuming its faster to do only 1 query)
+    const data = [
+      URL,
+      cookies[i].name,
+      cookies[i].value,
+      cookies[i].domain,
+      cookies[i].path, 
+      cookies[i].expires, 
+      cookies[i].size, 
+      cookies[i].httpOnly, 
+      cookies[i].secure, 
+      cookies[i].session, 
+      cookies[i].sameSite, 
+      cookies[i].sameParty, 
+      cookies[i].sourceScheme, 
+      cookies[i].sourcePort
+    ];
+
+    connection.query(insertDataQuery, data, (error, results) => {
+      if (error) {
+        console.error('Error inserting data: ', error);
+      } else {
+        console.log('Data inserted successfully!');
+      }
+    });
+  }
+}
+
+
+module.exports = {
+    establishConnection,
+    endConnection,
+  };
--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 const selectWebsites = require('./websiteSelection.cjs');
 const createBrowser = require('./browser');
+const databaseAPI = require('./db');
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const mysql = require('mysql2');
+
 
 // register `puppeteer-extra` plugins.
 puppeteer.use(StealthPlugin()) // allows to pass all tests on SannySoft, even if not *actually* in headfull mode
 
-const NUM_URLS = 10;
+const crawlID = Date.now()
+const NUM_URLS = 5;
 const browser_list = ['Chrome']
 
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'I@mastrongpsswd',
+    database: 'CrawlData',
+  });
+
+
+
 async function crawl(browser_list){
+    // set up database connection
+    connection.connect(function(err) {
+        if (err) {
+          return console.error('error: ' + err.message);
+        }
+      
+        console.log('Connected to the MySQL server.');
+      });
+
     // set up URL List
-    // URL_list = await selectWebsites.getFirstURLs(NUM_URLS);
-    URL_list = ["https://bot.sannysoft.com/"];
+    const URL_list = await selectWebsites.getFirstURLs(NUM_URLS);
+    // const URL_list = ["https://edition.cnn.com/"];
     
     for(let j = 0; j < browser_list.length; j++){
-        const browserInstance = await createBrowser.browserObject(browser_list[j]);
+        const browserInstance = await puppeteer.launch({
+            headless: false, //when false, allows to pass most tests on SannySoft, except WebDrive
+            // ignoreHTTPSErrors: true, // would this be desirable? -- allows you to visit websites that aren’t hosted over a secure HTTPS protocol and ignore any HTTPS-related errors.
+            args: [
+                '--start-maximized'// browser takes whole screen. 
+                // --proxy-server = x.x.x.x:xxxx // this can be used to specify the IP address
+            ] 
+        });
+    
         const page = await browserInstance.newPage();
 
         // loop through URLs
@@ -29,15 +59,53 @@ async function crawl(browser_list){
                 });
                 
                 await page.screenshot({path: "./screenshots/"+i+".png"});
+                let pageCookies = await page.cookies();
+                // console.log(pageCookies);
+
+                // await databaseAPI.saveCookies(URL, pageCookies);
+                // find how to put this in the db.js file and transfer the connection
+                const insertDataQuery = 'INSERT INTO cookie_data (crawlID, URL, name, value, domain, path, expires, size, httpOnly, secure, session, sameSite, sameParty, sourceScheme, sourcePort) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+                  
+                for(let i = 0; i < pageCookies.length; i++){ // later on see if we can do a "batch add" and add all the lines at once (assuming its faster to do only 1 query)
+                    const data = [
+                        crawlID,
+                        URL,
+                        pageCookies[i].name,
+                        pageCookies[i].value,
+                        pageCookies[i].domain,
+                        pageCookies[i].path, 
+                        pageCookies[i].expires, 
+                        pageCookies[i].size, 
+                        pageCookies[i].httpOnly, 
+                        pageCookies[i].secure, 
+                        pageCookies[i].session, 
+                        pageCookies[i].sameSite, 
+                        pageCookies[i].sameParty, 
+                        pageCookies[i].sourceScheme, 
+                        pageCookies[i].sourcePort
+                        ];
+                
+                    connection.query(insertDataQuery, data, (error, results) => {
+                    if (error) {
+                        console.error('Error inserting data: ', error);
+                    } else {
+                        console.log('Data inserted successfully!');
+                    }
+                    });
+                }
             } catch(error){
-                console.log("Couldn't open" + URL);
                 console.log(error);
             }
         }
-
-        await browserInstance.close();
+    
+    await browserInstance.close();
     }
+    connection.end(function(err) {
+        if (err) {
+          return console.log('error:' + err.message);
+        }
+        console.log('Close the database connection.');
+      });
 }
-
 
 crawl(browser_list);
