bcbb0fd7d79de12412de458f319460cbc591b52c
PhilippePH <55930314+PhilippePH@users.noreply.github.com>
2023-07-12 12:27:51 +0100

    Able to test for each category. Re-organisation of code in smaller functions for readability

--- bannerID/wordCounter.py
+++ bannerID/wordCounter.py
@@ -47,12 +47,121 @@ INFORMATION = ['learn more', 'more options', 'show purposes', 'more information'
 POLICY = ['privacy policy', 'privacy statement', 'cookie policy','cookie notice',]
 THIRD_PARTY = ['our partners', 'partners', 'vendors', 'third-party', 'third party']
 OTHER = ['similar technologies', 'other technologies',]
-ALLOWED_EXPRESSIONS = [CONSENT, NON_CONSENT, TYPE_OF_COOKIES, SETTINGS, INFORMATION,
-                       POLICY,THIRD_PARTY, OTHER]
+ALLOWED_EXPRESSIONS = { "consent": CONSENT,
+                       "non-consent": NON_CONSENT,
+                       "type of cookies":TYPE_OF_COOKIES, 
+                       "settings":SETTINGS, 
+                       "information":INFORMATION,
+                       "policy": POLICY,
+                       "third_party":THIRD_PARTY, 
+                       "other":OTHER }
 
 path = "bannerID/top20banners/train_files"
 directory = os.fsencode(path)
 
+""" Now, the next interesting thing would be to
+1) Test how many banners have at least one hit per category
+2) See if some words in the category could be removed (too few hits)"""
+
+def clean_word(search_term):
+    if len(search_term) <= 1:
+        return None
+    
+    # remove first or last char if it isn't alpha
+    while not search_term[0].isalpha():
+        search_term = search_term[1:]
+        if len(search_term) <= 1:
+            return None       
+    
+    while not search_term[-1].isalpha():
+        search_term = search_term[:-1]
+        if len(search_term) <= 1:
+            return None
+    
+    search_term = search_term.lower()
+
+    if search_term in IGNORE_WORDS:
+        return None
+
+    return search_term                    
+
+def increment_df(df, search_term, filename):
+    # Get word's row index      
+    row_index = df.index[df['word'] == search_term][0]
+
+    # If first occurence of the word in a new file
+    if df.at[row_index, filename] == 0:
+        df.at[row_index, filename] = 1
+        df.at[row_index, 'numFiles'] += 1
+        df.at[row_index, 'totalWordCount'] += 1
+
+    else:
+        df.at[row_index, filename] = 1
+        df.at[row_index, 'totalWordCount'] += 1
+                    
+def add_row_to_df(df, search_term, filename):
+    new_row = pd.Series({'word': search_term, 'numFiles': 1, 'totalWordCount': 1, filename:1})
+    df = pd.concat([df, new_row.to_frame().T], ignore_index=True)
+
+def modify_df(df, search_term, filename):
+    # Check if the term is found in the dataframe, if found increment
+    if search_term in df['word'].values:      
+        increment_df(df, search_term, filename)
+    # If not found, add a row to df
+    else: 
+        add_row_to_df(df, search_term, filename)
+
+def get_word_count(category_name = None, category_words = None):
+    df = pd.DataFrame(columns=['word','numFiles','totalWordCount'])
+    unique_file_counter = 0
+
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        complete_filename = path+'/'+filename
+        
+        with open(complete_filename, "r") as file:
+            # add a column to df, with default value = 0 (all words so far have frequency of 0)
+            df[filename] = 0
+            previous_word = None
+            file_first_hit = True
+
+            for line in file:
+                words = line.split()  # Split line into a list of words
+                for search_term in words:
+                    search_term = clean_word(search_term)
+                    if search_term is None:
+                        continue
+                    
+                    if previous_word:
+                        if (previous_word+" "+search_term) in category_words:
+                            words.append(previous_word+" "+search_term)
+                    
+                    if category_name:
+                        if search_term in category_words: #Only count words in the list given
+                            modify_df(df, search_term, filename)
+
+                            if file_first_hit:
+                                unique_file_counter += 1
+                                file_first_hit = False
+                    else:  
+                        modify_df(df, search_term, filename)
+
+                    previous_word = search_term
+
+    create_filename = 'bannerID/top20banners/results.csv'
+
+    if category_name:
+        create_filename = f'bannerID/top20banners/output_categories/{category_name}.csv'
+        success = round(unique_file_counter / 75 * 100, 2) # AS OF JULY 12, THERE ARE 75 TRAINING FILES
+        print(f"At least one word of the category {category_name} has been found in {unique_file_counter} files. ({success} %)")
+
+    df.to_csv(create_filename)
+
+for category_name, category_words in ALLOWED_EXPRESSIONS.items():
+    get_word_count(category_name, category_words)
+
+
+"""
 def select_testing_files():
     for file in os.listdir(directory):
         if random.randint(0,100) < 20:
@@ -61,87 +170,4 @@ def select_testing_files():
             new_path = "bannerID/top20banners/test_files"
             new_filename = new_path+'/'+filename
             os.rename(complete_filename, new_filename)
-
-
-def find_word_count():
-    df = pd.DataFrame(columns=['word','numFiles','totalWordCount'])
-
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        print(filename)
-        complete_filename = path+'/'+filename
-        
-        with open(complete_filename, "r") as file:
-            # add a column to df, with default value = 0 (all words so far have frequency of 0)
-            df[filename] = 0
-            previous_word = None
-
-            for line in file:
-                words = line.split()  # Split line into a list of words
-                for search_term in words:
-                    if previous_word:
-                        if (previous_word+" "+search_term) in ALLOWED_EXPRESSIONS:
-                            words.append(previous_word+" "+search_term)
-
-                    if len(search_term) <= 1:
-                        continue
-
-                    # remove first or last char if it isn't alpha
-                    while not search_term[0].isalpha():
-                        search_term = search_term[1:]
-                        if len(search_term) <= 1:
-                            break
-                    
-                    if len(search_term) <= 1:
-                        continue
-                    
-                    while not search_term[-1].isalpha():
-                        search_term = search_term[:-1]
-                        if len(search_term) <= 1:
-                            break
-                    
-                    if len(search_term) <= 1:
-                        continue
-                    
-                    search_term = search_term.lower() # remove any uppercase
-                    if search_term in IGNORE_WORDS: # WE SHOULD HAVE TWO WORD COMBINATIONS "MORE" is bad "FIND MORE" is ok
-                        continue
-
-                    # Check if the term is found in the dataframe, if found increment
-                    if search_term in df['word'].values:      
-
-                        # Get its row index      
-                        row_index = df.index[df['word'] == search_term][0]
-
-                        # If first occurence of the word in a new file
-                        if df.at[row_index, filename] == 0:
-                            df.at[row_index, filename] = 1
-                            df.at[row_index, 'numFiles'] += 1
-                            df.at[row_index, 'totalWordCount'] += 1
-                    
-                        else:
-                            df.at[row_index, filename] = 1
-                            df.at[row_index, 'totalWordCount'] += 1
-                    
-                    else:
-                        # If word not found, add a new row with it and set initial values
-                        # new_row = pd.Series([0] * len(df.columns))
-                        # new_row[0] = search_term
-                        # new_row[1] = 1
-                        # new_row[2] = 1
-                        # new_row[len(df.columns)] = 1
-                        # df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
-
-                        new_row = pd.Series({'word': search_term, 'numFiles': 1, 'totalWordCount': 1, filename:1})
-                        # new_row = pd.concat([new_row, pd.Series([0] * (len(df.columns) - len(new_row)))], ignore_index=True)
-                        df = pd.concat([df, new_row.to_frame().T], ignore_index=True)
-                    
-                    previous_word = search_term
-
-    df.to_csv('bannerID/top20banners/results.csv')
-
-# find_word_count()
-
-    
-
-    
+"""
