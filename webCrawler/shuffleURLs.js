adfee9553c360ae4d40af4a007359b18057c4253
PhilippePH <55930314+PhilippePH@users.noreply.github.com>
2023-08-09 17:37:09 +0100

    Created shuffled URL list

---
+++ webCrawler/shuffleURLs.js
@@ -0,0 +1,56 @@
+import fs from 'fs/promises';
+import {createWriteStream} from 'fs';
+import { CSVtoArray } from './websiteSelection.js';
+
+// shuffle code from: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+async function shuffle(array) {
+    let currentIndex = array.length;
+    let randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // Swap
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+
+async function randomiseURLs(path){
+    /* This function breaks down the input into three thirds.
+        The values within each thirds are shuffled.
+        They are then put back into one list, with a value from the first third,
+        second third and third third being added in order (and so on).
+      
+        This outputs a txt file with one URL on each line. */
+  
+      let myURLs = await CSVtoArray(path);
+      
+      let thirdLength = myURLs.length / 3;
+      
+      let firstThird = myURLs.slice(0, thirdLength);
+      let secondThird = myURLs.slice(thirdLength, thirdLength * 2);
+      let thirdThird = myURLs.slice(thirdLength * 2, myURLs.length);
+  
+      firstThird = await shuffle(firstThird);
+      secondThird = await shuffle(secondThird);
+      thirdThird = await shuffle(thirdThird);
+  
+      var file = createWriteStream('webCrawler/shuffled.txt');
+  
+      file.on('error', function(err) { console.log(err); return; });
+      for(let i = 0; i < thirdLength; i++){
+        await file.write(firstThird[i] + '\n');
+        await file.write(secondThird[i] + '\n');
+        await file.write(thirdThird[i] + '\n');
+      }
+      file.end();
+  }
+  
+  randomiseURLs("./webCrawler/top-100k.csv");
--- webCrawler/websiteSelection.js
+++ webCrawler/websiteSelection.js
@@ -1,36 +1,14 @@
-import fs from 'fs';
+import {createReadStream} from 'fs';
 import {parse} from "csv-parse";
 
-
-async function randomiseURLs(path){
-  /* This function breaks down the input into three thirds.
-      The values within each thirds are shuffled.
-      They are then put back into one list, with a value from the first third,
-      second third and third third being added in order (and so on).
-    
-      This outputs a txt file with one URL on each line. */
-
-    let myURLs = await CSVtoArray(path);
-    
-    let thirdLength = myURLs.length / 3;
-    
-    let firstThird = myURLs.slice(0, thirdLength);
-    let secondThird = myURLs.slice(thirdLength + 1, thirdLength * 2);
-    let thirdThird = myURLs.slice(thirdLength * 2 + 1, myURLs.length);
-
-    
-}
-
-// randomiseURLs("./webCrawler/top-1m.csv");
-
-async function CSVtoArray(path){
+export async function CSVtoArray(path){
   /* Read in a CSV file at path path.
     Modifies URL format to "https://www.sitename.tld"
     Returns the array of URLs. */
 
   return new Promise((resolve, reject) => {
     let myURLs = [];
-    fs.createReadStream(path)
+    createReadStream(path)
       .pipe(parse({ delimiter: ","}))
       .on("data", function (row) {
         myURLs.push("https://www."+row[1]);
@@ -59,5 +37,3 @@ export async function getSiteNames(url){
   const nameArray = await url.split(".");
   return nameArray[1];
 }
-
-// module.exports = {getFirstURLs, getSiteNames};
