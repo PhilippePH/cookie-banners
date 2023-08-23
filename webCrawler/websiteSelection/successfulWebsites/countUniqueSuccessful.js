7a2359f3eaabea858f98d104898c1cb234ecd1fa
Philippe PH <55930314+PhilippePH@users.noreply.github.com>
2023-08-23 11:24:17 +0100

    countUniqueSuccessful.js working

--- webCrawler/websiteSelection/successfulWebsites/countUniqueSuccessful.js
+++ webCrawler/websiteSelection/successfulWebsites/countUniqueSuccessful.js
@@ -1,6 +1,30 @@
-import { TXTtoArray } from '../websiteSelection.js'
+import { createReadStream } from 'fs'
 
-const browser = 'Brave'
+const browser = 'Firefox'
+
+ async function TXTtoArray (path) {
+  return new Promise((resolve, reject) => {
+    const myURLs = []
+    createReadStream(path, 'utf8')
+      .on('data', function (chunk) {
+        // Split the chunk into lines and process each line
+        const lines = chunk.split('\n')
+        lines.forEach(line => {
+          if (line !== '') { // Exclude empty site names
+            myURLs.push(line)
+          }
+        })
+      })
+      .on('end', function () {
+        resolve(myURLs)
+      })
+      .on('error', function (error) {
+        console.log(error.message)
+        reject(error)
+      })
+  })
+}
+
 
 async function main () {
   let path;
@@ -10,7 +34,6 @@ async function main () {
   else if (browser == 'Google Chrome') {path = './Google ChromeSuccessful.txt';}
   
   const resultsArr = await TXTtoArray(path)
-  console.log(resultsArr)
   const resultsSet = new Set(resultsArr)
 
   console.log(`${resultsSet.size} successful websites for ${browser}`)
